90e49cdcc9f7890cc2ab81be8b74ecb1397f0b09
RomanticD <96178954+RomanticD@users.noreply.github.com>
2023-11-17 10:43:29 +0800

    Initial Commit

---
+++ CurrencyExchangeSys/CurrencyExchangeSysApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeSysApp.swift
+//  CurrencyExchangeSys
+//
+//  Created by Romantic D on 2023/11/17.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeSysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
