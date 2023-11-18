86d4b60a5dce587bd7894fe938c7a7362de9ac81
RomanticD <96178954+RomanticD@users.noreply.github.com>
2023-11-18 13:41:52 +0800

    Add Animation

---
+++ CurrencyExchangeSys/OperationPanel.swift
@@ -0,0 +1,18 @@
+//
+//  OperationPanel.swift
+//  CurrencyExchangeSys
+//
+//  Created by Romantic D on 2023/11/18.
+//
+
+import SwiftUI
+
+struct OperationPanel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OperationPanel()
+}
