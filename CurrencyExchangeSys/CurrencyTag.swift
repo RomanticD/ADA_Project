a0d0873d84783e9f2b27ff8281fc95e9dbd903dc
RomanticD <96178954+RomanticD@users.noreply.github.com>
2023-11-19 21:38:32 +0800

    add API support

---
+++ CurrencyExchangeSys/CurrencyTag.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyTag.swift
+//  CurrencyExchangeSys
+//
+//  Created by Romantic D on 2023/11/19.
+//
+
+import SwiftUI
+
+struct CurrencyTag: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrencyTag()
+}
---
+++ CurrencyExchangeSys/FetchedExchageRate.swift
@@ -0,0 +1,8 @@
+//
+//  FetchedExchageRate.swift
+//  CurrencyExchangeSys
+//
+//  Created by Romantic D on 2023/11/18.
+//
+
+import Foundation
